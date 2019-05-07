b0ffbb2b1cf67fb4f9d038c8aca9632fc1e325f5
First_sight_407 <31948527+wangpengnovos@users.noreply.github.com>
2019-05-07 23:19:03 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#pathway enrichment analysis
-Rscript caculate_reporter_score.R --group group.list --abund KO.rela.xls --type pathway --outdir pathway
-Rscript plot_reporter.score.R pathway/reporter_score.xls pathway/
-
-#module enrichment analysis
-Rscript caculate_reporter_score.R --group group.list --abund KO.rela.xls --type module --outdir module
-Rscript plot_reporter.score.R module/reporter_score.xls module/
